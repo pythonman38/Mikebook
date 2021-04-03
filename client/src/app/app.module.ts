47c481828a1214e0ae8ce17d706192c373b11659
Michael Walker <michaelwalker846@yahoo.com>
2021-04-03 07:11:22 -0400

    Section 5 - Added navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
